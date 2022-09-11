f49815eff4bedcbca520a09502ada35770c540cd
JC <withj1123@gmail.com>
2022-09-11 20:47:54 +0900

    3.3.1 update

--- lib/screen/test_screen.dart
+++ lib/screen/test_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class TestScreen extends StatelessWidget {
+  const TestScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +9,6 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Container(
-          color: Colors.black,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
